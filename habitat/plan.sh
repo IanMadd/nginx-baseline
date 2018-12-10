a17fdcb4f4567080c88d07f7bb0eae1c89d9bf9e
Nell Shamrell <nellshamrell@gmail.com>
2018-12-10 14:26:39 -0800

    use updated hab inspec integration
    Signed-off-by: Nell Shamrell <nellshamrell@gmail.com>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,25 +1,30 @@
 pkg_name=inspec-profile-nginx-baseline
 pkg_version=2.2.0
 pkg_origin=nshamrell
-pkg_deps=(chef/inspec core/ruby core/hab)
-pkg_svc_user=root
+pkg_deps=(chef/inspec)
 pkg_license='Apache-2.0'
 
+do_setup_environment() {
+  ARCHIVE_PATH="$HAB_CACHE_SRC_PATH/$pkg_dirname/$pkg_name-$pkg_version.tar.gz"
+}
 
 do_build() {
-  cp -vr $PLAN_CONTEXT/../* $HAB_CACHE_SRC_PATH/$pkg_dirname
+  if [ ! -f $PLAN_CONTEXT/../inspec.yml ]; then
+    exit_with 'Cannot find inspec.yml. Please build from profile root.' 1
+  fi
+
+  local profile_files=($(ls $PLAN_CONTEXT/../ -I habitat -I results))
+  local profile_location="$HAB_CACHE_SRC_PATH/$pkg_dirname/build"
+  mkdir -p $profile_location
+
+  build_line "Copying profile files to $profile_location"
+  cp -R ${profile_files[@]} $profile_location
+
+  build_line "Archiving $ARCHIVE_PATH"
+  inspec archive "$HAB_CACHE_SRC_PATH/$pkg_dirname/build"                            -o $ARCHIVE_PATH                            --overwrite
 }
 
 do_install() {
-  local profile_contents
-  local excludes
-  profile_contents=($(ls))
-  excludes=(habitat results *.hart)
-
-  for item in ${excludes[@]}; do
-    profile_contents=(${profile_contents[@]/$item/})
-  done
-
-  mkdir ${pkg_prefix}/dist
-  cp -r ${profile_contents[@]} ${pkg_prefix}/dist/
+  mkdir -p $pkg_prefix/profiles
+  cp $ARCHIVE_PATH $pkg_prefix/profiles
 }
